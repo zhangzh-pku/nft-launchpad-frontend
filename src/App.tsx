482340eea650ac4ea0a2bb2f4a256a6e47014a0d
zhangzhihao <1900017707@pku.edu.cn>
2023-07-27 16:29:19 +0800

    feat: bid

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import ResponsiveAppBar from "./components/ui/AppBar";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Bid from "./components/Bid";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#000000", // TODO theme
+    },
+  },
+  typography: {
+    fontFamily: "Roboto",
+  },
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <Router>
+          <ResponsiveAppBar />
+          <Routes>
+            <Route path="/" Component={Bid}/>
+            </Routes>
+          <Bid />
+        </Router>
+      </div>
+    </ThemeProvider>
   );
 }
 
